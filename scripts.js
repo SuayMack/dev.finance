03ce4f218e027351b061219ef1f51bb19d185103
SuayMack <pry_chu@hotmail.com>
2021-02-16 18:33:23 -0300

    update income

--- scripts.js
+++ scripts.js
@@ -96,7 +96,7 @@ const DOM = {
 
       const html = `         
          <td class="description">${transaction.description}</td>
-         <td class="${CSSclass}">- ${amount}</td>
+         <td class="${CSSclass}">- R$ ${amount}</td>
          <td class="date">${transaction.date}</td>
          <td>
             <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação.">
